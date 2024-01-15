27db7a987b84fb4aa0a81d8b8e42a456e74f8d8b
Luca10992 <lucalillo.99.1@gmail.com>
2024-01-15 17:56:04 +0100

    fix. edit html structure

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const slideContainer = document.querySelector("container-primary");
+
+const slides = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
+
+for (let i = 0; i < slides.length; i++) {
+    const slide = slides[i];
+    
+}
