4a02ea1e4545e995553e537d617db55920b3f8f5
Luca10992 <lucalillo.99.1@gmail.com>
2024-01-16 11:32:58 +0100

    feat. add bonus 1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const slideContainer = document.querySelector(".container-primary");
 const arrowLeft = document.querySelector(".arrow-left");
 const arrowRight = document.querySelector(".arrow-right");
+const miniatureContainer = document.querySelector(".miniature-container")
 
 // definisco le varie slide
 const slides = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
+let slideBig = 0;
 
 // definisco la slide mostrata 
 let slideHtml = '';
@@ -17,23 +19,44 @@ for (let i = 0; i < slides.length; i++) {
 }
 slideContainer.innerHTML += slideHtml;
 
+// genero le miniature
+let miniHtml = '';
+for (let i = 0; i < slides.length; i++) {
+    const miniature = slides[i];
+    let miniActive = (i === slideBig) ? "active" : "";
+    miniHtml += `
+    <div class="slide-container">
+        <img class="miniature ${miniActive}" src="./img/${miniature}" alt="slide ${i}">
+    </div>
+    `
+}
+miniatureContainer.innerHTML = miniHtml;
+
 // * evento freccia destra
 const allSlides = document.querySelectorAll(".slide");
-let slideBig = 0;
+const allMiniature = document.querySelectorAll(".miniature");
 arrowRight.addEventListener('click', function () {
     allSlides[slideBig].classList.remove("active"); 
+    allMiniature[slideBig].classList.remove("active"); 
     slideBig++;
     if (slideBig >= allSlides.length) slideBig = 0;
 
     allSlides[slideBig].classList.add("active");
+    allMiniature[slideBig].classList.add("active");
 });
 
 // * evento freccia sinistra
 arrowLeft.addEventListener('click', function () {
     allSlides[slideBig].classList.remove("active"); 
+    allMiniature[slideBig].classList.remove("active"); 
     slideBig--;
     if (slideBig < 0 ) slideBig = 4;
 
     allSlides[slideBig].classList.add("active");
+    allMiniature[slideBig].classList.add("active");
 });
+
+// * evento click miniature
+
+
 
